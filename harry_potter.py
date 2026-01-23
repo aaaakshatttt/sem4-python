9c7008b9b33dffc42860d12bb5137626996e8549
aaaakshatttt <veeakshat4@gmail.com>
2026-01-23 19:04:30 +0530

    A PROJECT

--- harry_potter.py
+++ harry_potter.py
@@ -1,12 +1,16 @@
+#declaring variables so each house have a box and later we will keep adding points
 Gryffindor = 0
 Ravenclaw  = 0
 Hufflepuff = 0
 Slytherin = 0
+#printed first qiuestion
 print("Q1) Do you like Dawn or Dusk?")
 print("1) Dawn")
 print("2) Dusk")
+#declaring a variable 'a' to store the input answer as digit so using int()and we talking input from user so input()
 a = int(input("enter your choice: "))
-if a == 1:
+#ifelse statements
+if a == 1: #if the choice is 1 so the variable gryffindor which originally had 0 points will now have 1
   Gryffindor = Gryffindor + 1
   Ravenclaw  = Ravenclaw + 1
 elif a == 2:
@@ -49,7 +53,8 @@ elif c == 4:
   Gryffindor = Gryffindor + 4
 else:
   print("wrong input")
-
+#after all the questions now we want to print all the points of all houses so in inverted commas print() prints whatever is written inside the commas but if i write print(griffindor) then the print() will print the value of the variable griffindor which we declared earlier but so do both
+#to do both i did print("Slytherin",Slytherin) so in inverted commas it printed whatever its wrutten and after coma it will print value of variable
 print("Slytherin",Slytherin)
 
 print("Hufflepuff",Hufflepuff) 
@@ -58,7 +63,17 @@ print("Ravenclaw ",Ravenclaw)
 
 print("Gryffindor",Gryffindor) 
 
+print("----------------------------------------")
+# the program also asked for bonus note if i can printy the variable with the highest points soooo
 
+if Slytherin > Hufflepuff  and Slytherin > Ravenclaw and Slytherin > Gryffindor:
+  print("Slytherin",Slytherin)
+elif Hufflepuff > Slytherin  and Hufflepuff > Ravenclaw and Hufflepuff > Gryffindor: 
+  print("Hufflepuff",Hufflepuff) 
+elif Ravenclaw > Hufflepuff  and Ravenclaw > Slytherin and Ravenclaw > Gryffindor:
+  print("Ravenclaw ",Ravenclaw)
+else:
+  print("Gryffindor",Gryffindor)
 
 
 
