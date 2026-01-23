198f58d3511dbbb9340c98aa6f5d0ba45afce98e
aaaakshatttt <veeakshat4@gmail.com>
2026-01-23 15:11:18 +0530

    variables finished

---
+++ debugging.py
@@ -0,0 +1,9 @@
+butterflies = 10
+beetles = 12
+ladybugs = 20
+total = butterflies+beetles+ladybugs
+
+print("i caught",int(butterflies),"butterflies")
+print("i caught",int(beetles),"beetles")
+print("i caught",int(ladybugs),"ladybugs")
+print('I caught ' + str(total) + ' total bugs!')
