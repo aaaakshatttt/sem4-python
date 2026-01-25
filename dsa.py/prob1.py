232e7b82203d0e5cd3493ef43e8e01cc07beeaa8
aaaakshatttt <veeakshat4@gmail.com>
2026-01-25 09:05:37 +0530

    started dsa

---
+++ dsa.py/prob1.py
@@ -0,0 +1,9 @@
+#print the larger number
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+if a > b:
+    print("largest number is",a)
+elif a < b:
+    print("largest number is",b)
+else:
+    print("they are equal")    
