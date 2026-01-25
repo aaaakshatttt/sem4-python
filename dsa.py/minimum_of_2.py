3d9b554881427e0c8956efd1cf3c5b5f1f8b1af5
aaaakshatttt <veeakshat4@gmail.com>
2026-01-25 09:06:00 +0530

    yeah

---
+++ dsa.py/minimum_of_2.py
@@ -0,0 +1,8 @@
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+if a > b:
+    print("minimum number is",b)
+elif b > a:
+    print("minimum number is",a)
+else:
+    print("the are equal")        
