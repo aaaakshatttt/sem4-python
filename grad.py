3ce3179089742424d4608d0057ac7238793b137f
aaaakshatttt <veeakshat4@gmail.com>
2026-01-26 19:56:14 +0530

    school

---
+++ grad.py
@@ -0,0 +1,10 @@
+s = int(input("enter grade: "))
+if s >= 90:
+    print("A")
+elif s <90 and s>80:
+    print("B")
+elif  s <=80 and s> 70:
+    print("C")
+else:
+    print("u fail")
+               
